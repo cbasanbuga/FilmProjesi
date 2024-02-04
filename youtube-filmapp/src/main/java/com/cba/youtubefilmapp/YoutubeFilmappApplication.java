a643fd30ff9008ebf7799d8e026e361de1f63b7a
CBA <asanbuga@gmail.com>
2024-02-04 23:45:46 +0300

    Main metodunun içi boşaltıldı

--- youtube-filmapp/src/main/java/com/cba/youtubefilmapp/YoutubeFilmappApplication.java
+++ youtube-filmapp/src/main/java/com/cba/youtubefilmapp/YoutubeFilmappApplication.java
@@ -3,14 +3,12 @@ package com.cba.youtubefilmapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.cba.youtubefilmapp.domains.Film;
-
 @SpringBootApplication
 public class YoutubeFilmappApplication {
 
 	public static void main(String[] args) {
 		
-		Film f = new Film();
+	
 		
 		
 	}
